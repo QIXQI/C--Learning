a719aaba655f9831c7bb50bb740d301a01e73aa9
qixqi <zhengxiang4056@gmail.com>
2020-05-20 02:13:41 +0800

    Say hello

---
+++ Say Hello/Say Hello/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace Say_Hello
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SayHelloButton = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // SayHelloButton
+            // 
+            this.SayHelloButton.Location = new System.Drawing.Point(266, 213);
+            this.SayHelloButton.Name = "SayHelloButton";
+            this.SayHelloButton.Size = new System.Drawing.Size(100, 23);
+            this.SayHelloButton.TabIndex = 0;
+            this.SayHelloButton.Text = "Say hello";
+            this.SayHelloButton.UseVisualStyleBackColor = true;
+            this.SayHelloButton.Click += new System.EventHandler(this.SayHelloButton_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("宋体", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.textBox1.Location = new System.Drawing.Point(241, 125);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(203, 44);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "Hello there!";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.SayHelloButton);
+            this.Name = "Form1";
+            this.Text = "Say hello";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button SayHelloButton;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Say Hello/Say Hello/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Say_Hello
+{
+    public partial class Form1 : Form
+    {
+        Random rand;        // 全局变量
+
+        public Form1()
+        {
+            InitializeComponent();
+            rand = new Random();        // 初始化
+        }
+
+        private byte[] GetRandomBytes(int n)
+        {
+            // n个长度的随机数字组成数组
+            var randomBytes = new byte[n];
+            rand.NextBytes(randomBytes);
+            return randomBytes;
+        }
+
+        private void SayHelloButton_Click(object sender, EventArgs e)
+        {
+            byte[] rgb = GetRandomBytes(3);
+            textBox1.ForeColor = Color.FromArgb(255, rgb[0], rgb[1], rgb[2]);
+        }
+    }
+}
